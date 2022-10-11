719088c8b3a0499c12b8f401d856831363dea052
Cuauhtemoc Corrales <cuauhtemoccorralesc@gmail.com>
2022-10-11 16:09:52 -0500

    Commit starter

---
+++ functions.py
@@ -0,0 +1,16 @@
+
+"""
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- project: Inversion de Capital por estrategia activa y pasiva                                                        -- #
+# -- script: functions.py : python script with general functions                                         -- #
+# -- author: CuauhtemocCC                                                                       -- #
+# -- license: GPL-3.0 License                                                                            -- #
+# -- repository: https://github.com/CuauhtemocCC/Microestructuras-de-Trading-Lab-1                                                                     -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
+import numpy as np
+from scipy.optimize import minimize
+
+
+
+
---
+++ visualizations.py
@@ -0,0 +1,15 @@
+
+"""
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- project: Inversion de Capital por estrategia activa y pasiva                                                        -- #
+# -- script: functions.py : python script with general functions                                         -- #
+# -- author: CuauhtemocCC                                                                       -- #
+# -- license: GPL-3.0 License                                                                            -- #
+# -- repository: https://github.com/CuauhtemocCC/Microestructuras-de-Trading-Lab-1                                                                     -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
+
+import pandas as pd
+import plotly.express as px
+import data as dt
+
