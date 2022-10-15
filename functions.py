650941ba3335bd15bd6c9ca08b463c51cca93205
Marco Ochoa <if720072@iteso.mx>
2022-10-15 14:41:06 -0500

    Primera funcion
    funcion de ayuda para leer los archivos cuando se tenga el main del equipo

--- functions.py
+++ functions.py
@@ -12,5 +12,20 @@ import numpy as np
 from scipy.optimize import minimize
 
 
+# Aqui voy a crear algo de pseudo codigo para el main en equipo por si es necsario donde se pueda leer el excel y se
+# haga el data frame
+
+def f_leer_archivo(ruta_archivo):
+    archivo = pd.read_csv(ruta_archivo, header=0, skip_blank_lines=True)
+    archivo = archivo.dropna().reset_index(drop=True)
+    archivo = archivo.rename(columns={'Price.1': 'Close Price'}, inplace=False)
+    archivo['Close Price'] = pd.to_numeric(archivo['Close Price'])
+    archivo['Price'] = pd.to_numeric(archivo['Price'])
+    archivo['Profit'] = [i.replace(" ", "") for i in archivo['Profit']]
+    archivo['Profit'] = pd.to_numeric(archivo['Profit'])
+    archivo['Item'] = [archivo['Item'].iloc[i].replace('-e', '') for i in range(len(archivo))]
+
+
+    return archivo
 
 
